2efe39dfb8f80891f2e32e3d7cc4e2986e0c01d5
Memon SK <ShuaibM@ais.ac.nz>
2021-02-12 17:38:08 +1300

    Shuaib V3

--- TestingJenkins.java
+++ TestingJenkins.java
@@ -3,6 +3,8 @@ public class TestingJenkins
 	public static void main(String[] args)
 	{
 	System.out.println("Welcome to Jenkins");
+
+	system.out.println("Testing error");
 	//semicolon is missing at the end of line;
 	}
 }
